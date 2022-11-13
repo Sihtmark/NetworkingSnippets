a8cc6c51f07575077498f9b959ac20200ef62d76
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-13 14:49:52 +0300

    very first snippet

--- NetworkingSnippets.playground/Contents.swift
+++ NetworkingSnippets.playground/Contents.swift
@@ -1,3 +1,56 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum AccountType: String, Codable {
+    case Banking
+    case CreditCard
+    case Investment
+}
+
+enum NetworkError: Error {
+    case serverError
+    case decodingError
+}
+
+struct Profile: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+}
+
+struct Account: Codable {
+    let id: String
+    let type: AccountType
+    let name: String
+    let amount: Decimal
+    let createdDateTime: Date
+}
+
+func fetchAccounts(forUserId userId: String, completion: @escaping (Result<[Account],NetworkError>) -> Void) {
+    let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)/accounts")!
+
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        DispatchQueue.main.async {
+            guard let data = data, error == nil else {
+                completion(.failure(.serverError))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+
+                let accounts = try decoder.decode([Account].self, from: data)
+                completion(.success(accounts))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }
+    }.resume()
+}
